c81306139a119cc630db0cae0d68f999d1b7c973
bryanchimborazo <80365041+bryanchimborazo@users.noreply.github.com>
2021-09-28 23:50:14 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,40 +1,6 @@
 //https://www.eclipse.org/paho/clients/js/
 
-// VARIABLES
-var operandoa;
-var operandob;
 
-function init() {
-  mostrarPrompt();
-}
-
-function mostrarPrompt() {
-  var pOperadora = prompt("Introduzca el primer operador.");
-  var pOperadorb = prompt("Introduzca el segundo operador.");
-
-  if (pOperadora == null || !pOperadora) {
-    operandoa = 0;
-  }
-
-  if (pOperadorb == null || pOperadorb) {
-    operandob = 0;
-  }
-
-  operandoa = Number(pOperadora);
-  operandob = Number(pOperadorb);
-
-  if (operandoa == null) {
-    operandoa = 0;
-  }
-
-  if (operandob == null) {
-    operandob = 0;
-  }
-
-  var result = operandoa + operandob;
-
-  console.log(operandoa, " + ", operandob, " = ", result);
-}
 
 function LED1_On() {
 	//alert("led on");
